c40d243065a9ae0d525a4306c73203d85b0edfc6
amalali8 <amalza958@gmail.com>
2023-08-10 17:41:13 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function FindSeven(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i].toString().includes('7')) {
+        return "Found!";
+      }
+    }
+    return "There is no 7 in the array.";
+  }
+
+  let n1 = [1, 2, 3, 4, 5];
+console.log(FindSeven(n1)); 
+let n2 = [7, 8, 9, 10];
+console.log(FindSeven(n2)); 
+
+let n3 = [8, 9, 10];
+console.log(FindSeven(n3));
